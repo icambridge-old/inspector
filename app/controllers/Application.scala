ccbcad98e137a3deb7ff7e1614111f9d61832c67
Iain Cambridge <github@icambridge.me>
2013-01-02 17:46:05 +0000

    UI tweaks

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,12 +3,12 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-import models.Posts
+import util.Location
 
 object Application extends Controller {
 
   def index = Action {
-
+    Location.set("Home")
     Ok(views.html.index("Your new application is ready!"))
   }
 
